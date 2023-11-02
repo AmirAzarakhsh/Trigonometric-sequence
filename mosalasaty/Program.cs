0feba519b7fa1589fa13535553631c59afb7c11f
LOQ <LOQ@DESKTOP-FUMHBU1>
2023-11-02 17:12:39 +0330

    Add project files.

---
+++ mosalasaty/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int n ;
+int m;
+Console.WriteLine("az chandomin adad mosalasaty shoru shavad");
+n = int.Parse(Console.ReadLine());
+Console.WriteLine("ta chandomin adad mosalasaty");
+m = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= m; i++) 
+{
+    int donbalemosalasaty = ((i) * (i + 1)) / 2;
+    Console.WriteLine("adad"+donbalemosalasaty);
+}
+// amirhossein azarakhsh tamrine6 //
+
+
+
